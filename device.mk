06d1280dad3c50bc40bd4730040814540c8b1da0
speedhorn <manishsuper577@gmail.com>
2020-08-25 11:37:45 +0000

    rosy: update some stuffs

--- device.mk
+++ device.mk
@@ -214,6 +214,7 @@ PRODUCT_PACKAGES += \
     init.class_main.sh \
     init.qcom.class_core.sh \
     init.qcom.early_boot.sh \
+    init.qcom.post_boot.sh \
     init.qcom.power.rc \
     init.qcom.rc \
     init.qcom.sensors.sh \
